971ec3ce1fdc5b22ea68fa6730e5b64286ddcb52
pirraste <pirraste@gmail.com>
2011-03-08 09:16:54 +0000

    added MIT license

--- jquery.gradable.js
+++ jquery.gradable.js
@@ -1,17 +1,34 @@
 /**
- * Another gradient plugin. It adds a gradient to the background of an element.
- *
- * @example $('div').gradable({ fromColor: '000000', toColor: '+3', direction: 'h' });
- *
- * @fromColor   [hex color value (#123456)]
- * @toColor     [hex color value (#123456)|rgb(11,22,33)|numeric delta (+10)]
- * @direction   [horizontal|h|vertical|v]
- *
- * @name gradable
- * @type jQuery
- * @cat Plugins/gradient
- * @author Stefano Pirra
+Another gradient plugin. It adds a gradient to the background of an element.
+
+@example: $('div').gradable({ fromColor: '000000', toColor: '+3', direction: 'h' });
+
+@fromColor:   [hex color value (#123456)]
+@toColor:     [hex color value (#123456)|rgb(11,22,33)|numeric delta (+10)]
+@direction:   [horizontal|h|vertical|v]
+ 
+ 
+Copyright (C) 2011 by Stefano Pirra
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
  */
+ 
 (function($){
     // default settings
     var defaults = {
